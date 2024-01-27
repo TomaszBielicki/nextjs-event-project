9a953ec9d4a6efae42156fc85c52ded928123459
Tomek <tomek.bielicki@gmail.com>
2024-01-27 20:25:59 +0100

    event item list created

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -5,9 +5,16 @@ function EventList(props) {
   const { items } = props;
   return (
     <ul>
-      {items.map((events) => (
+      {items.map((event) => (
         <li>
-          <EventItem />
+          <EventItem
+            key={event.id}
+            id={event.id}
+            title={event.title}
+            location={event.location}
+            date={event.date}
+            image={event.image}
+          />
         </li>
       ))}
     </ul>
