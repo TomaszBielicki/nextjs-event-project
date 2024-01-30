4e7c153195bdd99e381f089d26ea2d5fa1d48fd5
Tomek <tomek.bielicki@gmail.com>
2024-01-30 21:36:53 +0100

    event item improved

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -2,7 +2,10 @@ import Link from "next/link";
 import React from "react";
 
 import classes from "./event-item.module.css";
-
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 function EventItem(props) {
   const { title, image, date, location, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -20,13 +23,20 @@ function EventItem(props) {
           <h2>{title}</h2>
         </div>
         <div className={classes.date}>
+          <DateIcon />
           <time>{humanReadableDate}</time>
         </div>
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
         <div className={classes.actions}>
-          <Link href={expolreLink}>Explore event</Link>
+          <Button link={expolreLink}>
+            <span>Expolre Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
