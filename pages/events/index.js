a53087081b522f9ed8c9755f68b45f32dbb2ea17
Tomek <tomek.bielicki@gmail.com>
2024-02-01 23:06:54 +0100

    Event search used

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,15 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 function EventsPage() {
-  return <div>EventsPage</div>;
+  const events = getAllEvents();
+  return (
+    <Fragment>
+      <EventsSearch />
+      <EventList items={events} />
+    </Fragment>
+  );
 }
 
 export default EventsPage;
